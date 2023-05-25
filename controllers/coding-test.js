558326fe83fb4448dfd294f324eeab698a8921d0
kyk <kyk4296@naver.com>
2023-05-25 17:08:41 +0900

    230525

--- controllers/coding-test.js
+++ controllers/coding-test.js
@@ -76,3 +76,119 @@ exports.transformString = async function (req, res) {
     return res.prg('오류가 발생 했습니다.', STATUS_CODE.FAILED);
   }
 };
+
+/**
+*  @swagger
+*  paths:
+*   /coding-test/schedule:
+*     get:
+*       summary: 시간표
+*       description: 시간표
+*       tags: [coding test (코딩 테스트)]
+*       responses:
+*         "200":
+*           description: OK
+*/
+exports.schedule = async function (req, res) {
+  
+  // let schedule = [
+  //   ["MO 12:00 WE 14:30", "MO 12:00", "MO 15:00", "MO 18:00"],
+  //   ["TU 09:00", "TU 10:00", "TU 15:00", "MO 18:00"],
+  //   ["WE 09:00", "WE 12:00", "WE 15:00", "WE 18:00"],
+  //   ["TH 09:30", "TH 11:30", "TH 15:00", "TH 18:00"],
+  //   ["FR 15:00", "FR 15:00", "FR 15:00", "FR 15:00"],
+  // ]
+
+  let schedule = [
+    ["MO 12:00 WE 14:30", "MO 12:00", "MO 15:00", "MO 18:00"],
+    ["TU 09:00", "TU 10:00", "TU 15:00", "MO 18:00"],
+    ["WE 09:00", "WE 12:00", "WE 15:00", "WE 18:00"],
+    ["TH 09:30", "TH 11:30", "TH 15:00", "TH 18:00"],
+    ["FR 15:00", "FR 15:00", "FR 15:00", "FR 15:00"],
+  ];
+  
+  function parseTime(timeStr) {
+    let [hour, minute] = timeStr.split(':').map(Number);
+    let result = hour * 60 + minute;
+
+    // console.log('parseTime result: ', result);
+    return result;
+  }
+  
+  function overlap(time1, time2) {
+    let [start1, end1] = time1;
+    let [start2, end2] = time2;
+    let result = (start1 < end2) && (end1 > start2);
+
+    // console.log('overlap result: ', result);
+    return result;
+  }
+  
+  let validSchedules = 0;
+  
+  function backtrack(timetable, index) {
+    // console.log('---------------');
+    // console.log(index);
+    // console.log(schedule.length);
+    // console.log('---------------');
+    if (index === schedule.length) {
+
+      
+      validSchedules += 1;
+      return;
+    }
+    
+    for (let i = 0; i < schedule[index].length; i++) {
+      let classTimes = schedule[index][i].split(' ');
+
+      // if(i === 0){
+      //   console.log(classTimes);
+      //   console.log(classTimes.length);
+      // }
+
+
+      let valid = true;
+      
+      for (let j = 0; j < classTimes.length; j += 2) {
+        let day = classTimes[j];
+        let start = parseTime(classTimes[j + 1]);
+        let end = start + (classTimes.length > 2 ? 90 : 180);
+        
+        if (timetable[day].some(time => overlap(time, [start, end]))) {
+          valid = false;
+          break;
+        }
+      }
+      
+      if (valid) {
+        for (let j = 0; j < classTimes.length; j += 2) {
+          let day = classTimes[j];
+          let start = parseTime(classTimes[j + 1]);
+          let end = start + (classTimes.length > 2 ? 90 : 180);
+          timetable[day].push([start, end]);
+        }
+        
+        backtrack(timetable, index + 1);
+        
+        for (let j = 0; j < classTimes.length; j += 2) {
+          let day = classTimes[j];
+          timetable[day].pop();
+        }
+      }
+    }
+  }
+  
+  let timetable = { 'MO': [], 'TU': [], 'WE': [], 'TH': [], 'FR': [] };
+  
+  backtrack(timetable, 0);
+  
+  console.log(validSchedules);
+
+  
+  let result = 1;
+  if(result){
+    return res.prg({result: result});
+  }else{
+    return res.prg('오류가 발생 했습니다.', STATUS_CODE.FAILED);
+  }
+};
--- routes/coding-test.js
+++ routes/coding-test.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const ctrlCodingTest = require('../controllers/coding-test');
 
 router.get('/transform-string', ctrlCodingTest.transformString); // 변형 문자열
+router.get('/schedule', ctrlCodingTest.schedule); // 시간표
 
 module.exports = router;
